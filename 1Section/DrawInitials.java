0b492fb43d00773899c6d3a96c534906da5e3f61
EC2 Default User <ec2-user@ip-172-31-5-181.us-east-2.compute.internal>
2023-03-28 13:15:39 +0000

    first section complete

---
+++ 1Section/DrawInitials.java
@@ -0,0 +1,13 @@
+public class DrawInitials {
+    
+    public static void main(String[] args) {
+        System.out.println("    J H   H W   W");
+        System.out.println("    J H   H W   W");
+        System.out.println("    J H   H W   W");
+        System.out.println("    J HHHHH W   W");
+        System.out.println("    J H   H W W W");
+        System.out.println("J   J H   H WW WW");
+        System.out.println("JJJJJ H   H W   W");
+    }
+    
+}
---
+++ 1Section/FirstProgram.java
@@ -0,0 +1,17 @@
+/* Write a program that prints this to the terminal:
+>> Printing is fun!
+>> Java > Python.
+>> I spilled Java all over my paper.
+>> My dog ate my Java.
+*/
+
+public class FirstProgram {
+    
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!");
+        System.out.println("Java > Python.");
+        System.out.println("I spilled Java all over my paper.");
+        System.out.println("My dog ate my Java.");
+    }
+    
+}
