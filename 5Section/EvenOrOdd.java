46caad040f5be41600f24dfa51e1755c4de25a08
Jason Whisnant <tasups@gmail.com>
2023-03-31 20:19:14 +0000

    workbook 5.6 done

---
+++ 5Section/EvenOrOdd.java
@@ -0,0 +1,15 @@
+public class EvenOrOdd {
+    
+    public static void main(String [] args) {
+        String even = "even";
+        String odd = "odd";
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + "--" + even);
+            } else {
+                System.out.println(i + "--"  + odd);
+            }
+        }
+    }
+    
+}
