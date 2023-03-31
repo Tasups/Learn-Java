d5bd298d9c4ea4f02517842f96c1298f15519c81
Jason Whisnant <tasups@gmail.com>
2023-03-31 18:07:18 +0000

    RockPaperScissorsLizardSpock complete

--- 4Section/RockPaperScissorsLizardSpock.java
+++ 4Section/RockPaperScissorsLizardSpock.java
@@ -33,10 +33,10 @@ public class RockPaperScissorsLizardSpock {
         } else if (num == 2){
             String piece = "paper";
             return piece;
-        } else if (num == 2){
+        } else if (num == 3){
             String piece = "scissors";
             return piece;
-        } else if (num == 2){
+        } else if (num == 4){
             String piece = "lizard";
             return piece;
         } else {
@@ -47,27 +47,76 @@ public class RockPaperScissorsLizardSpock {
     
     public static String whoWon(String computerChoice, String playerChoice) {
         String result = "";
+        // ties
         if (computerChoice == "rock" && playerChoice == "rock" || computerChoice == "paper" && playerChoice == "paper" || computerChoice == "scissors" && playerChoice == "scissors" || computerChoice == "lizard" && playerChoice == "lizard" || computerChoice == "Spock" && playerChoice == "Spock") {
             result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". The result is a tie.";
-        } else if (computerChoice == "rock" && playerChoice == "scissors" || computerChoice == "rock" && playerChoice == "lizard") {
+        } 
+        /******  COMPUTER WINS GAME  ********/ 
+        // computer picks rock and wins
+        else if (computerChoice == "rock" && playerChoice == "scissors" || computerChoice == "rock" && playerChoice == "lizard") {
             result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since rock crushes "  + playerChoice + ", you lose!";
-        } else if (computerChoice == "paper" && playerChoice == "rock" || computerChoice == "paper" && playerChoice == "Spock") {
+        } 
+        // computer picks paper and wins
+        else if (computerChoice == "paper" && playerChoice == "rock" || computerChoice == "paper" && playerChoice == "Spock") {
             if (playerChoice == "rock") { 
                 result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since paper covers rock, you lose!";
             } else {
                 result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since paper disproves Spock, you lose!";
             }
+        // computer pickes scissors and wins
         } else if (computerChoice == "scissors" && playerChoice == "paper" || computerChoice == "scissors" && playerChoice == "lizard") {
             if (playerChoice == "paper") { 
                 result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since scissors cuts paper, you lose!";
             } else {
                 result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since scissors decapitates lizard, you lose!";
             }
+        // computer picks lizard and wins
         } else if (computerChoice == "lizard" && playerChoice == "paper" || computerChoice == "lizard" && playerChoice == "Spock") {
             if (playerChoice == "paper") { 
                 result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since lizard eats paper, you lose!";
             } else {
                 result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since lizard poisons Spock, you lose!";
+            }
+        // computer picks Spock and wins
+        } else if (computerChoice == "Spock" && playerChoice == "scissors" || computerChoice == "Spock" && playerChoice == "rock") {
+            if (playerChoice == "scissors") { 
+                result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since Spock smashes scissors, you lose!";
+            } else {
+                result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since Spock vaporizes rock, you lose!";
+            }
+        } 
+        /******  PLAYER WINS GAME  ********/ 
+        // player picks rock and wins
+        else if (computerChoice == "scissors" && playerChoice == "rock" || computerChoice == "lizard" && playerChoice == "rock") {
+            result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since rock crushes "  + playerChoice + ", you win!";
+        } 
+        // player picks paper and wins
+        else if (computerChoice == "rock" && playerChoice == "paper" || computerChoice == "Spock" && playerChoice == "paper") {
+            if (computerChoice == "rock") { 
+                result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since paper covers rock, you win!";
+            } else {
+                result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since paper disproves Spock, you win!";
+            }
+        // player pickes scissors and wins
+        } else if (computerChoice == "paper" && playerChoice == "scissors" || computerChoice == "lizard" && playerChoice == "scissors") {
+            if (computerChoice == "paper") { 
+                result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since scissors cuts paper, you win!";
+            } else {
+                result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since scissors decapitates lizard, you win!";
+            }
+        // player picks lizard and wins
+        } else if (computerChoice == "paper" && playerChoice == "lizard" || computerChoice == "Spock" && playerChoice == "lizard") {
+            if (computerChoice == "paper") { 
+                result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since lizard eats paper, you win!";
+            } else {
+                result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since lizard poisons Spock, you win!";
+            }
+        // player picks Spock and wins
+        } else if (computerChoice == "scissors" && playerChoice == "Spock" || computerChoice == "rock" && playerChoice == "Spock") {
+            if (computerChoice == "scissors") { 
+                result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since Spock smashes scissors, you win!";
+            } else {
+                result = "You chose " + playerChoice + " and the computer chose " + computerChoice + ". Since Spock vaporizes rock, you win!";
             }
         }
         return result;
