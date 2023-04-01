5ab9412fdc1150298e37a7d4bc52f2958cf32eb1
Jason Whisnant <tasups@gmail.com>
2023-04-01 01:15:14 +0000

    workbook 5.10 done

--- 5Section/RollDoubles.java
+++ 5Section/RollDoubles.java
@@ -2,16 +2,22 @@ public class RollDoubles {
     
     public static void main(String[] args) {
         
-        int rollOne = RNG();
-        int rollTwo = RNG();
+        int rollOne = 0;
+        int rollTwo = 1;
         
-        System.out.println("Die 1: " + rollOne);
-        System.out.println("Die 2: " + rollTwo);
+        while(rollOne != rollTwo) {
+            rollOne = RNG();
+            rollTwo = RNG();
+            System.out.println("Die 1: " + rollOne);
+            System.out.println("Die 2: " + rollTwo + "\n");
+        }
+        
+        System.out.println("You rolled doubles!");
         
     }
     
     public static int RNG() {
-        double randomDouble = Math.random() * 6;
+        double randomDouble = Math.random() * 6 + 1;
         int randomInt = (int)randomDouble;
         return randomInt;
     }
