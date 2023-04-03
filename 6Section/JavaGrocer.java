51b17a48e8e5f694554995743023cfee23cc4c64
Jason Whisnant <tasups@gmail.com>
2023-04-03 21:09:05 +0000

    workbook 6.4 done

--- 6Section/JavaGrocer.java
+++ 6Section/JavaGrocer.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
+
 public class JavaGrocer {
     
     public static void main(String[] args) {
         String[] aisles = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
-        System.out.println("\nDo you sell coffee?");
+        System.out.println("\nWhat can I help you find?");
+        
+        Scanner scan = new Scanner(System.in);
+        
+        String item = scan.nextLine();
         
         for (int i = 0; i < aisles.length; i++) {
-            if (aisles[i] == "coffee") {
+            if (aisles[i].equals(item)) {
                 System.out.println("\nWe have that in aisle: " + i + ".");
             }
         }
+        
+        scan.close();
     }
     
 }
