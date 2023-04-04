98a7908a6e82ba452c7446ceffde2084cc20a49d
Jason Whisnant <tasups@gmail.com>
2023-04-04 19:34:33 +0000

    workbook 6.13 revisited done

--- 6Section/TwoDArrays.java
+++ 6Section/TwoDArrays.java
@@ -11,28 +11,25 @@ public class TwoDArrays {
             {95, 98, 99, 100}
         };
         
-        System.out.println("Harry: " + Arrays.toString(easyArrayOfGrades[0]));
-        System.out.println("Ron: " + Arrays.toString(easyArrayOfGrades[1]));
-        System.out.println("Hermione: " + Arrays.toString(easyArrayOfGrades[2]));
+        // System.out.println("Harry: " + Arrays.toString(easyArrayOfGrades[0]));
+        // System.out.println("Ron: " + Arrays.toString(easyArrayOfGrades[1]));
+        // System.out.println("Hermione: " + Arrays.toString(easyArrayOfGrades[2]));
         
-        System.out.println("Harry's grades: Potions " + easyArrayOfGrades[0][0] + ", Herbology " + easyArrayOfGrades[0][1] + ", Arithmancy " + easyArrayOfGrades[0][2] + ", and Charms " + easyArrayOfGrades[0][3] + ".");
-        
-        grades[0][0] = 72;
-        grades[0][1] = 74;
-        grades[0][2] = 78;
-        grades[0][3] = 76;
-        grades[1][0] = 65;
-        grades[1][1] = 64;
-        grades[1][2] = 61;
-        grades[1][3] = 67;
-        grades[2][0] = 95;
-        grades[2][1] = 98;
-        grades[2][2] = 99;
-        grades[2][3] = 100;
-        
-        // System.out.println("Harry: " + Arrays.toString(grades[0]));
-        // System.out.println("Ron: " + Arrays.toString(grades[1]));
-        // System.out.println("Hermione: " + Arrays.toString(grades[2]));
+        for (int i = 0; i < easyArrayOfGrades.length; i++) {
+            
+            switch (i) {
+                case 0: System.out.print("Harry: "); break;
+                case 1: System.out.print("Ron: "); break;
+                case 2: System.out.print("Hermione: "); break;
+                default: System.out.print("Error"); break;
+            }
+            
+            for (int j = 0; j < easyArrayOfGrades[i].length; j++) {
+                System.out.print(easyArrayOfGrades[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+      
         
 
     }
