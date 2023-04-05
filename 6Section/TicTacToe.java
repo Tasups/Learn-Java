30f5779fe8f7f800ca56791486e8274b38227977
Jason Whisnant <tasups@gmail.com>
2023-04-05 15:17:01 +0000

    winner functionality works, now for error statement and looping when a selection is made that has already been made.

--- 6Section/TicTacToe.java
+++ 6Section/TicTacToe.java
@@ -13,28 +13,35 @@ public class TicTacToe {
         
         printBoard(ticTacToeBoard);
         
-        boolean winner = false;
+        String winner = null;
         
         Scanner scan = new Scanner(System.in);
         
         int row;
         int column;
 
-        while (!winner) {
+        while (winner == null) {
             System.out.println("Turn: X");
-            System.out.print("- pick a row and column number: ");
-            row = scan.nextInt();
-            column = scan.nextInt();
+            System.out.println("- pick a row and column number: ");
+            row = scan.nextInt() - 1;
+            column = scan.nextInt() - 1;
+            System.out.println("\n");
             ticTacToeBoard[row][column] = " X ";
             printBoard(ticTacToeBoard);
-            
+            winner = checkWinner(ticTacToeBoard);
+            if (winner != null) {break;}
             System.out.println("Turn: O");
-            System.out.print("- pick a row and column number: ");
-            row = scan.nextInt();
-            column = scan.nextInt();
+            System.out.println("- pick a row and column number: ");
+            row = scan.nextInt() - 1;
+            column = scan.nextInt() - 1;
+            System.out.println("\n");
             ticTacToeBoard[row][column] = " O ";
             printBoard(ticTacToeBoard);
+            winner = checkWinner(ticTacToeBoard);
+            if (winner != null) {break;}
         }
+        
+        System.out.println("Congratulations!" + winner + "is the winner!!");
      
         scan.close();
         
@@ -59,4 +66,68 @@ public class TicTacToe {
         }
     }
     
+    public static String checkWinner(String[][] board) {
+        String X = " X ";
+        String O = " O ";
+        // Horizontal win scenarios
+        if (board[0][0] == X && board[0][1] == X && board[0][2] == X) {
+            return X;
+        } else if (board[0][0] == O && board[0][1] == O && board[0][2] == O) {
+            return O;
+        } else if (board[1][0] == X && board[1][1] == X && board[1][2] == X) {
+            return X;
+        } else if (board[1][0] == O && board[1][1] == O && board[1][2] == O) {
+            return O;
+        } else if (board[2][0] == X && board[2][1] == X && board[2][2] == X) {
+            return X;
+        } else if (board[2][0] == O && board[2][1] == O && board[2][2] == O) {
+            return O;
+        }
+        // Vertical win scenarios
+          else if (board[0][0] == X && board[1][0] == X && board[2][0] == X) {
+            return X;
+        } else if (board[0][0] == O && board[1][0] == O && board[2][0] == O) {
+            return O;
+        } else if (board[0][1] == X && board[1][1] == X && board[2][1] == X) {
+            return X;
+        } else if (board[0][1] == O && board[1][1] == O && board[2][1] == O) {
+            return O;
+        } else if (board[0][2] == X && board[1][2] == X && board[2][2] == X) {
+            return X;
+        } else if (board[0][2] == O && board[1][2] == O && board[2][2] == O) {
+            return O;
+        }
+        // Diagonal win scenarios
+          else if (board[0][0] == X && board[1][1] == X && board[2][2] == X) {
+            return X;
+        } else if (board[0][0] == O && board[1][1] == O && board[2][2] == O) {
+            return O;
+        } else if (board[0][2] == X && board[1][1] == X && board[2][0] == X) {
+            return X;
+        } else if (board[0][2] == O && board[1][1] == O && board[2][0] == O) {
+            return O;
+        } else {
+            return null;
+        }
+    }
+    
+    // This is a check of the position to see if it's been taken, this functionality is to be implemented after the game is functional with indicating winner
+    public static boolean checkPosition(String[][] board, int x, int y) {
+        if (board[x][y] != " _ ") {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    
+    
 }
+
+            // THIS IS A CHECK TO SEE IF THE POSITION IS TAKEN, SOMETHING TO WORK ON IN THE NEAR FUTURE, LET'S DO WIN CONDITION FIRST!
+            // if(checkPosition(ticTacToeBoard, row, column)){
+                
+            // } else {
+            //     System.out.println("That position is taken. Please choose another.");
+            //     // then we need to return to X selection point
+            // }
