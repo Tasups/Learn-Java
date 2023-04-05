0cdf6b1995b1b31a5463e7ef43e2799bf900d1ef
Jason Whisnant <tasups@gmail.com>
2023-04-05 16:39:47 +0000

    ticTacToe game done, except how to make scan stay on one line with multiple iterations

--- 6Section/TicTacToe.java
+++ 6Section/TicTacToe.java
@@ -26,15 +26,29 @@ public class TicTacToe {
             row = scan.nextInt() - 1;
             column = scan.nextInt() - 1;
             System.out.println("\n");
+            while (ticTacToeBoard[row][column] != " _ ") {
+                System.out.println("That position is taken, try again: ");
+                row = scan.nextInt() - 1;
+                column = scan.nextInt() - 1;
+                System.out.println("\n");
+            }
             ticTacToeBoard[row][column] = " X ";
+            
             printBoard(ticTacToeBoard);
             winner = checkWinner(ticTacToeBoard);
             if (winner != null) {break;}
+            
             System.out.println("Turn: O");
             System.out.println("- pick a row and column number: ");
             row = scan.nextInt() - 1;
             column = scan.nextInt() - 1;
             System.out.println("\n");
+            while (ticTacToeBoard[row][column] != " _ ") {
+                System.out.println("That position is taken, try again: ");
+                row = scan.nextInt() - 1;
+                column = scan.nextInt() - 1;
+                System.out.println("\n");
+            }
             ticTacToeBoard[row][column] = " O ";
             printBoard(ticTacToeBoard);
             winner = checkWinner(ticTacToeBoard);
@@ -111,23 +125,5 @@ public class TicTacToe {
         }
     }
     
-    // This is a check of the position to see if it's been taken, this functionality is to be implemented after the game is functional with indicating winner
-    public static boolean checkPosition(String[][] board, int x, int y) {
-        if (board[x][y] != " _ ") {
-            return false;
-        } else {
-            return true;
-        }
-    }
-    
-    
     
 }
-
-            // THIS IS A CHECK TO SEE IF THE POSITION IS TAKEN, SOMETHING TO WORK ON IN THE NEAR FUTURE, LET'S DO WIN CONDITION FIRST!
-            // if(checkPosition(ticTacToeBoard, row, column)){
-                
-            // } else {
-            //     System.out.println("That position is taken. Please choose another.");
-            //     // then we need to return to X selection point
-            // }
