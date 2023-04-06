694d89b05c01e684eab5d71ca4a8dd361743f026
Jason Whisnant <tasups@gmail.com>
2023-04-06 20:12:45 +0000

    workbook 7.1 done

---
+++ 7Section/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Car toyota = new Car("Toyota", 25_000, 2022, "silver");
+        Car honda = new Car("Honda", 35_000, 2021, "black");
+        
+        System.out.println("This " + toyota.make + " is worth $" + toyota.price + 
+        ". It was built in " + toyota.year + ". It is " + toyota.color + ".\n");
+        System.out.println("This " + honda.make + " is worth $" + honda.price + 
+        ". It was built in " + honda.year + ". It is " + honda.color + ".\n");
+        
+        Person jason = new Person();
+        jason.name = "Jason Whisnant";
+        jason.nationality = "United States of America";
+        jason.dateOfBirth = "08/24/1977";
+        jason.passport = new String[]{jason.name, jason.nationality, jason.dateOfBirth};
+        jason.seatNumber = 17;
+        
+        System.out.println(jason.name);
+        System.out.println(jason.nationality);
+        System.out.println(Arrays.toString(jason.passport));
+        System.out.println(jason.dateOfBirth);
+        System.out.println(jason.seatNumber);
+    }
+    
+}
