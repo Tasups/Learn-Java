07258a140961f8e3ed560f63b72b8456562f1616
Jason Whisnant <tasups@gmail.com>
2023-04-07 19:39:18 +0000

    workbook 7.3 done

--- 7Section/Main.java
+++ 7Section/Main.java
@@ -4,19 +4,19 @@ public class Main {
     
     public static void main(String[] args) {
         
-        Car toyota = new Car("Toyota", 25_000, 2022, "silver");
-        Car honda = new Car("Honda", 35_000, 2021, "black");
+        // Car toyota = new Car("Toyota", 25_000, 2022, "silver");
+        // Car honda = new Car("Honda", 35_000, 2021, "black");
         
-        toyota.setColor("Jet Black");
-        toyota.setPrice(1000);
-        toyota.setYear(2001);
-        toyota.setMake("Nissan");
+        // toyota.setColor("Jet Black");
+        // toyota.setPrice(1000);
+        // toyota.setYear(2001);
+        // toyota.setMake("Nissan");
         
         
-        System.out.println("This " + toyota.getMake() + " is worth $" + toyota.getPrice() + 
-        ". It was built in " + toyota.getYear() + ". It is " + toyota.getColor() + ".\n");
-        System.out.println("This " + honda.getMake() + " is worth $" + honda.getPrice() + 
-        ". It was built in " + honda.getYear() + ". It is " + honda.getColor() + ".\n");
+        // System.out.println("This " + toyota.getMake() + " is worth $" + toyota.getPrice() + 
+        // ". It was built in " + toyota.getYear() + ". It is " + toyota.getColor() + ".\n");
+        // System.out.println("This " + honda.getMake() + " is worth $" + honda.getPrice() + 
+        // ". It was built in " + honda.getYear() + ". It is " + honda.getColor() + ".\n");
         
         // Person jason = new Person();
         // jason.name = "Jason Whisnant";
@@ -25,15 +25,13 @@ public class Main {
         // jason.passport = new String[]{jason.name, jason.nationality, jason.dateOfBirth};
         // jason.seatNumber = 17;
         
-        // System.out.println(jason.name);
-        // System.out.println(jason.nationality);
-        // System.out.println(Arrays.toString(jason.passport));
-        // System.out.println(jason.dateOfBirth);
-        // System.out.println(jason.seatNumber);
+        Person jason = new Person("Jason Whisnant", "Paraguan", "08/24/1977", 1);
         
-        Dude jason = new Dude("Jason Whisnant", "Paraguan", "08/24/1977", 1);
+        jason.setSeatNumber(10);
         
-        // System.out.println("I am " + jason.name + ". I am a " + jason.nationality + " born in " + jason.dateOfBirth + ". Please let me sit in seat number: " + jason.seatNumber);
+        // jason.passport = new String[]{jason.name, jason.nationality, jason.dateOfBirth};
+        
+        System.out.println("I am " + jason.getName() + ". I am a " + jason.getNationality() + " born in " + jason.getDateOfBirth() + ". Please let me sit in seat number: " + jason.getSeatNumber());
     }
     
 }
