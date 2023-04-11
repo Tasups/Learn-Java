805801ca2be52d95939671d0c8f2a892730bc4c8
Jason Whisnant <tasups@gmail.com>
2023-04-11 19:45:51 +0000

    workbook 7.8 done

--- 7Section/Main.java
+++ 7Section/Main.java
@@ -3,6 +3,18 @@ import java.util.Arrays;
 public class Main {
     
     public static void main(String[] args) {
+        
+        Car[] cars = new Car[] {
+            new Car("Nissan", 5000, 2020, "red", new String[] {"tires", "keys"}),
+            new Car("Dodge", 8500, 2019, "blue", new String[] {"tires", "keys"}),
+            new Car("Nissan", 5000, 2017, "yellow", new String[] {"tires", "filter"}),
+            new Car("Honda", 7000, 2019, "orange", new String[] {"tires", "filter"}),
+            new Car("Mercedes", 12000, 2015, "jet black", new String[] {"tires", "filter", "transmission"})
+        };
+        
+        Dealership dealership = new Dealership(cars);
+        
+        
      
         // String[] spareParts = {"Tires", "Keys"};
      
@@ -17,28 +29,28 @@ public class Main {
         
         
         
-        String[] newPassport = new String[]{"Jason Whisnant", "Paraguan", "08/24/1977"};
+        // String[] newPassport = new String[]{"Jason Whisnant", "Paraguan", "08/24/1977"};
        
-        Person jason = new Person("Jason Whisnant", "Ashevillean", "08/24/1977", 17);
+        // Person jason = new Person("Jason Whisnant", "Ashevillean", "08/24/1977", 17);
         
-        System.out.println("Name: " + jason.getName());
-        System.out.println("Nationality: " + jason.getNationality());
-        System.out.println("Date of Birth: " + jason.getDateOfBirth());
-        System.out.println("Seat Number: " + jason.getSeatNumber());
+        // System.out.println("Name: " + jason.getName());
+        // System.out.println("Nationality: " + jason.getNationality());
+        // System.out.println("Date of Birth: " + jason.getDateOfBirth());
+        // System.out.println("Seat Number: " + jason.getSeatNumber());
         
-        boolean passport = jason.applyPassport();
-        if(passport) {
-            jason.setPassport();
-            System.out.println("Congratulations, " + jason.getName() + ". Your passport was approved!");
-            System.out.println("Passport: " + Arrays.toString(jason.getPassport()));
-            System.out.println("I'm sorry, your seat was taken, we will have to assign you another...");
-            int seatChoice = jason.chooseSeat();
-            System.out.println("We were able to secure another seat for you. You will be seated at seat: " + seatChoice + ". We hope you have a safe flight.");
-        } else {
-            System.out.println("We are sorry " + jason.getName() + ". We cannot process your application.");
-        }
+        // boolean passport = jason.applyPassport();
+        // if(passport) {
+        //     jason.setPassport();
+        //     System.out.println("Congratulations, " + jason.getName() + ". Your passport was approved!");
+        //     System.out.println("Passport: " + Arrays.toString(jason.getPassport()));
+        //     System.out.println("I'm sorry, your seat was taken, we will have to assign you another...");
+        //     int seatChoice = jason.chooseSeat();
+        //     System.out.println("We were able to secure another seat for you. You will be seated at seat: " + seatChoice + ". We hope you have a safe flight.");
+        // } else {
+        //     System.out.println("We are sorry " + jason.getName() + ". We cannot process your application.");
+        // }
         
-        System.out.println(jason);
+        // System.out.println(jason);
         
         
         // Person twin = new Person(jason);
