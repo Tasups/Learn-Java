94f11dcc8382e9b4a404ce0de883a3f769cc110e
Jason Whisnant <tasups@gmail.com>
2023-04-17 19:19:19 +0000

    Exception Handling Challenge part 2 done

---
+++ 10Section/ExtraPractice/Magazine.java
@@ -0,0 +1,10 @@
+public class Magazine {
+    
+    private String author;
+    private String publisher;
+    private int issueNumber;
+    private int publicationYear;
+    
+    public Magazine(String author, String)
+    
+}
