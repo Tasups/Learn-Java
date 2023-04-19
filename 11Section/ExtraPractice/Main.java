8f95206182e0480bde6116090a36bfb1b9013b40
Jason Whisnant <tasups@gmail.com>
2023-04-19 12:57:18 +0000

    Map Collections Extra Practice part 3 complete, testing passed

--- 11Section/ExtraPractice/Main.java
+++ 11Section/ExtraPractice/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
@@ -23,24 +25,88 @@ public class Main {
         
         /* PART TWO VALIDATION *********************************************************/
         
-        CityPopulationTracker tracker = new CityPopulationTracker();
+        // CityPopulationTracker tracker = new CityPopulationTracker();
         
-        // // Test the addCity method
-        tracker.addCity(new City("New York", "USA", 8550405));
-        tracker.addCity(new City("Los Angeles", "USA", 3971883));
+        // // // Test the addCity method
+        // tracker.addCity(new City("New York", "USA", 8550405));
+        // tracker.addCity(new City("Los Angeles", "USA", 3971883));
     
-        // // Test the getCity method
-        City nyCity = tracker.getCity("New York");
-        System.out.println(nyCity.toString());
-        System.out.println("Population of New York: " + nyCity.getPopulation());
+        // // // Test the getCity method
+        // City nyCity = tracker.getCity("New York");
+        // System.out.println(nyCity.toString());
+        // System.out.println("Population of New York: " + nyCity.getPopulation());
     
-        // // Test the setCity method
-        City updatedNyCity = new City("New York", "USA", 8600000);
-        tracker.setCity(updatedNyCity);
+        // // // Test the setCity method
+        // City updatedNyCity = new City("New York", "USA", 8600000);
+        // tracker.setCity(updatedNyCity);
     
-        // // Verify that the city data was updated
-        nyCity = tracker.getCity("New York");
-        System.out.println("Updated population of New York: " + nyCity.getPopulation());
+        // // // Verify that the city data was updated
+        // nyCity = tracker.getCity("New York");
+        // System.out.println("Updated population of New York: " + nyCity.getPopulation());
+        
+        
+        /* PART THREE VALIDATION *********************************************************/
+        
+        Scanner scanner = new Scanner(System.in);
+
+        String cityName = promptForCityName(scanner);
+
+        String country = promptForCountry(scanner);
+
+        int population = promptForPopulation(scanner);
+
+        City newCity = new City(cityName, country, population);
+
+        CityPopulationTracker tracker = new CityPopulationTracker();
+        tracker.addCity(newCity);
+
+        System.out.println("City added to the tracker: " + newCity.getName());
+        System.out.println(newCity.toString());
+        
+        scanner.close();
+        
+    }
+    
+    public static String promptForCityName(Scanner scanner) {
+        while (true) {
+            System.out.println("\nPlease enter a valid city name: ");
+            String cityName = scanner.nextLine();
+            if (!isNullOrBlank(cityName)) {
+                return cityName;
+            }
+        }
+    }
+    
+    public static String promptForCountry(Scanner scanner) {
+        while (true) {
+            System.out.println("\nPlease enter a valid country name: ");
+            String country = scanner.nextLine();
+            if (!isNullOrBlank(country)) {
+                return country;
+            }
+        }
+    }
+    
+    public static int promptForPopulation(Scanner scanner) {
+        while (true) {
+            System.out.println("\nPlease enter a valid population: ");
+            if (!scanner.hasNextInt()) {
+                scanner.next();
+                continue;
+            }
+            int population = scanner.nextInt();
+            if(!incorrectPopulation(population)) {
+                return population;
+            }
+        }
+    }
+    
+    public static boolean isNullOrBlank(String input) {
+        return (input == null || input.isBlank());
+    }
+    
+    public static boolean incorrectPopulation(int population) {
+        return (population <= 0);
     }
     
 }
