48dadcce403e81f7ffa95378ab2893423a5fea1b
Jason Whisnant <tasups@gmail.com>
2023-04-19 20:19:23 +0000

    Enums Extra Practice part 2 done, passing tests

--- 14Section/ExtraPractice/Main.java
+++ 14Section/ExtraPractice/Main.java
@@ -1,13 +1,16 @@
 import model.Car;
+import model.CarDealership;
 import static model.Car.BodyType.*;
+
 
 public class Main {
     
     public static void main(String[] args) {
         
-        Car car1 = new Car("Toyota", "Camry", SEDAN, 2020, 30000);
-        System.out.println(car1.getMake());
-        System.out.println(car1.toString());
+        /* PART 1 TESTS **************************************************************/
+        // Car car1 = new Car("Toyota", "Camry", SEDAN, 2020, 30000);
+        // System.out.println(car1.getMake());
+        // System.out.println(car1.toString());
     
         // Uncomment the following line to test invalid input for setMake method
         // car1.setMake("");
@@ -29,6 +32,28 @@ public class Main {
     
         // Uncomment the following line to test invalid input for the constructor
         // Car car2 = new Car("", "Camry", SEDAN, 2020, 30000);
+        
+        /* PART 2 TESTS **************************************************************/
+        Car car1 = new Car("Toyota", "Camry", SEDAN, 2020, 30000);
+        Car car2 = new Car("Honda", "Civic", SEDAN, 2021, 25000);
+    
+        CarDealership dealership = new CarDealership();
+        
+        // Test the addCar method
+        dealership.addCar(car1);
+        dealership.addCar(car2);
+    
+        // Test the getCar method
+        Car retrievedCar = dealership.getCar(0);
+        System.out.println(retrievedCar.getMake() + " " + retrievedCar.getModel());
+    
+        // Test the setCar method
+        Car newCar = new Car("Ford", "Mustang", COUPE, 2022, 45000);
+        dealership.setCar(0, newCar);
+    
+        // Verify that the car was updated
+        retrievedCar = dealership.getCar(0);
+        System.out.println(retrievedCar.getMake() + " " + retrievedCar.getModel());
         
     }
     
--- 14Section/ExtraPractice/model/CarDealership.java
+++ 14Section/ExtraPractice/model/CarDealership.java
@@ -1,5 +1,28 @@
 package model;
+import java.util.List;
+import java.util.ArrayList;
 
 public class CarDealership {
-
+    
+    private ArrayList<Car> cars; 
+    
+    public CarDealership() {
+        this.cars = new ArrayList<>();
+    }
+    
+    public Car getCar(int index) {
+        Car carCopy = new Car(this.cars.get(index));
+        return carCopy;
+    }
+    
+    public void setCar(int index, Car newCar) {
+        Car carCopy = new Car(newCar);
+        this.cars.set(index, carCopy);
+    }
+    
+    public void addCar(Car newCar) {
+        Car carCopy = new Car(newCar);
+        this.cars.add(carCopy);
+    }
+    
 }
