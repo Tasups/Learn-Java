81c4c00d540f90a21b55e15cd61fba013d84b48a
Jason Whisnant <tasups@gmail.com>
2023-04-29 00:04:40 +0000

    Capstone part2 done, tests passed

--- Capstone/Main.java
+++ Capstone/Main.java
@@ -1,6 +1,7 @@
 import pojo.MarginAccount;
 import pojo.TradeAccount;
 import pojo.CashAccount;
+import repository.TradeAccountRepository;
 import java.math.BigDecimal;
 
 
@@ -8,6 +9,9 @@ public class Main {
     
     public static void main(String[] args) {
         
+        //FIRST PART TESTS
+        
+        /*  
         CashAccount cashAccount = new CashAccount("C123", new BigDecimal("1000.00"));
         System.out.println("Cash Account Details:");
         System.out.println("ID: " + cashAccount.getId());
@@ -29,6 +33,25 @@ public class Main {
         System.out.println("Cloned Margin Account Details:");
         System.out.println("ID: " + clonedMarginAccount.getId());
         System.out.println("Margin: " + clonedMarginAccount.getMargin());
+        */
+        
+        // SECOND PART TESTS
+        
+        TradeAccountRepository repository = new TradeAccountRepository();
+
+        CashAccount cashAccount = new CashAccount("C123", new BigDecimal("1000.00"));
+        repository.createTradeAccount(cashAccount);
+
+        MarginAccount marginAccount = new MarginAccount("M456", new BigDecimal("5000.00"));
+        repository.createTradeAccount(marginAccount);
+
+        TradeAccount retrievedCashAccount = repository.retrieveTradeAccount("C123");
+        System.out.println("Retrieved Cash Account ID: " + retrievedCashAccount.getId());
+        System.out.println("Cash Balance: " + ((CashAccount) retrievedCashAccount).getCashBalance());
+
+        TradeAccount retrievedMarginAccount = repository.retrieveTradeAccount("M456");
+        System.out.println("Retrieved Margin Account ID: " + retrievedMarginAccount.getId());
+        System.out.println("Margin: " + ((MarginAccount)retrievedMarginAccount).getMargin());
         
     }
     
--- Capstone/repository/TradeAccountRepository.java
+++ Capstone/repository/TradeAccountRepository.java
@@ -1,7 +1,29 @@
 package repository;
 
+import java.util.Map;
+import java.util.HashMap;
+
+import pojo.TradeAccount;
+
+
 public class TradeAccountRepository {
     
+    private Map<String, TradeAccount> datastore = new HashMap<>();
     
+    public void createTradeAccount(TradeAccount tradeAccount) {
+        this.datastore.put(tradeAccount.getId(), tradeAccount.clone());
+    }
+    
+    public TradeAccount retrieveTradeAccount(String id) {
+        return this.datastore.get(id).clone();
+    }
+    
+    public void updateTradeAccount(TradeAccount tradeAccount) {
+        this.datastore.put(tradeAccount.getId(), tradeAccount.clone());
+    }
+    
+    public void deleteTradeAccount(String id) {
+        this.datastore.remove(id);
+    }
     
 }
